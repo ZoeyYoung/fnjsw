9c3efa40f9c4c39f0d2b304daf06f82718f4c648
Frank <frankchen1328@gmail.com>
2014-10-29 21:42:33 +0800

    修改，查询区划联动
    修改，查询区划联动

--- src/main/java/fnjsw/component/impl/DivisionComponentImpl.java
+++ src/main/java/fnjsw/component/impl/DivisionComponentImpl.java
@@ -23,25 +23,42 @@ public class DivisionComponentImpl {
     @Autowired
     private DivisionMapper divisionMapper;
 
-    public List<Division> getByCode(String code) {
+    public List<Division> getByCode(String code){
         DivisionExample example = new DivisionExample();
         DivisionExample.Criteria criteria = example.createCriteria();
-        // 大于等于两个0的情况
-        String regex = "0{2,}";
-        Pattern p = Pattern.compile(regex);
-        Matcher m = p.matcher(code);
-        // result 为前面非0的数字
-        String result = m.replaceAll("").trim();
-        // sql like 为 12%0000... 后面的零的个数，通过如下的 sb 获得
-        int origLength = result.length();
-        int zeroFill = 15 - 2 - origLength;
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < zeroFill; i++) {
-            sb.append("0");
-        }
+        // 
+        String query = getTrimResult(code);
         criteria.andDivcodeNotEqualTo(code);
-        criteria.andDivcodeLike(result + "%" + sb.toString());
+        criteria.andDivcodeLike(query);
         return divisionMapper.selectByExample(example);
+    }
+
+    private String getTrimResult(String code) {
+        if (code == null || "".equals(code)) {
+            return "%0000000000000"; // 13个0
+        }
+        String str02 = code.substring(0, 2);
+        String str24 = code.substring(2, 4);
+        String str46 = code.substring(4, 6);
+        String str69 = code.substring(6, 9);
+        String str912 = code.substring(9, 12);
+        // String str1215 = code.substring(12, 15);
+        if (str02.equals("00")) {
+            return code;
+        }
+        if (str24.equals("00")) {
+            return str02 + "%00000000000"; // 11 个0
+        }
+        if (str46.equals("00")) {
+            return str02 + str24 + "%000000000"; // 9个0
+        }
+        if (str69.equals("000")) {
+            return str02 + str24 + str46 + "%000000"; // 6个0
+        }
+        if (str912.equals("000")) {
+            return str02 + str24 + str46 + str69 + "%000"; // 3个0
+        }
+        return code;
     }
 
     public int save(Division Division) {
